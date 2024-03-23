fc9081935ee9e6b1c1ae5ec5fb8221edfff7fded
Motocarota <motocarota@gmail.com>
2024-03-23 14:03:24 +0100

    fix img src

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -4,6 +4,7 @@
 <section
   id="top"
   class="h-[600px] mx-auto"
+  style="background-image: url(img/delfino.webp);"
 >
   <img src="img/title.webp" alt="delfino bianco" class="title"/>
 </section>
