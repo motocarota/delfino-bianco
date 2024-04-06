ff169529a2ffa0bda475d1b3d07aa62132a89345
Motocarota <motocarota@gmail.com>
2024-04-06 11:57:11 +0200

    no features

--- src/lib/Features.svelte
+++ src/lib/Features.svelte
@@ -6,12 +6,12 @@
     <div>
       <img src="img/sestri.webp" alt="sestri" style="width: 100%" />
     </div>
-    <div class="flex justify-center gap-4 flex-wrap max-w-screen-lg	mx-auto py-5">
+    <!-- <div class="flex justify-center gap-4 flex-wrap max-w-screen-lg	mx-auto py-5">
       <div class="w-33">Eventi</div>
       <div class="w-33">Banchetti</div>
       <div class="w-33">Cocktails</div>
       <div class="w-33">Cerimonie</div>
       <div class="w-33">Aperitivi</div>
       <div class="w-33">Lunch Menu</div>
-    </div>
+    </div> -->
   </section>
