a2d1cb8a32b69793882ec9b77a5e1ad4eb8b2dcb
Motocarota <motocarota@gmail.com>
2024-04-13 23:51:56 +0200

    mc

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -6,12 +6,13 @@
   <div class="max-w-screen-lg py-10 mx-auto font-thin text-center">
     <h1 class="mb-4">Delfino Bianco</h1>
     <p class="mb-6 text-xl">Ristorante Bar Pizzeria</p>
+    <SocialLinks />
+
     <p>via XX Settembre 12, 16039 Sestri Levante (Ge) ITALY</p>
     <p>Tel. +39 0185450837 - 328 1467737</p>
     <p>
       <a href="mailto:ristorantedelfinobianco@gmail.com?subject=informazioni" target="_blank">ristorantedelfinobianco@gmail.com</a>
     </p>
     <p>www.ristorantedelfinobianco.it</p>
-    <SocialLinks />
   </div>
 </footer>
