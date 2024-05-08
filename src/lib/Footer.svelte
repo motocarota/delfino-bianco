ad50f00947d167c744afe3b5f8c8a0031363b31b
Motocarota <motocarota@gmail.com>
2024-05-08 19:05:32 +0200

    adds cf rea cu

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,6 +2,7 @@
   <div class="max-w-screen-lg py-10 mx-auto font-thin text-center">
     <h1 class="mb-4">Delfino Bianco</h1>
     <p class="mb-6 text-xl">Ristorante Bar Pizzeria</p>
+    <p>www.ristorantedelfinobianco.it</p>
     <p>
       Tel. <a href="tel:00390185450837">+39.0185.450.837</a> -
       <a href="tel:00393281467737">328.1467737</a>
@@ -9,6 +10,8 @@
     <p>
       <a href="mailto:ristorantedelfinobianco@gmail.com?subject=informazioni" target="_blank">ristorantedelfinobianco@gmail.com</a>
     </p>
-    <p>www.ristorantedelfinobianco.it</p>
+    <br>
+    <p>P.IVA e C.F. 02391980998 - REA 482546</p>
+    <p>Codice Univoco M5UXCR1</p>
   </div>
 </footer>
