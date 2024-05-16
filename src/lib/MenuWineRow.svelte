78547a2a1e47e736849a12f482c0b3393178cb9e
Motocarota <motocarota@gmail.com>
2024-05-16 13:14:00 +0200

    adds wine menu

---
+++ src/lib/MenuWineRow.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let post;
+  const [
+    name, type, canteen, location, price
+  ] = post ?? [];
+</script>
+
+<tr>
+  {#if !price}
+    <th colspan="3" class="pt-5 text-xl">{name}</th>
+  {:else}
+    <td><span class="text-xl">{name}</span> {type} - {canteen} - {location}</td>
+    <td>{price}</td>
+  {/if}
+</tr>
+
+<style>
+  th {
+    font-weight: 900;
+  }
+  td {
+    text-align: left;
+  }
+</style>
---
+++ src/routes/menu-vini/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount } from "svelte";
+  import MenuWineRow from "../../lib/MenuWineRow.svelte";
+  const endpoint = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsqIM7bI1QSk4J7bJlAzgvUks8ZtzXrOSNezIQ5kYo7Cu69pcq9o4sFwX5_cnwbotWwOaSYFoVlVMA/pub?gid=0&single=true&output=tsv";
+  let posts = [];
+  let loading = true;
+
+  onMount(async function () {
+    const response = await fetch(endpoint);
+    const data = await response.text();
+    
+		posts = (data.split('\r\n')).map(row => row.split('\t'));
+    loading = false;
+  });
+
+</script>
+
+<main>
+  <h1>Delfino Bianco</h1>
+  <h3>Carta dei vini</h3>
+  <img src="img/liguria.webp" alt="liguria" />
+  <table>
+    {#if loading}
+      <p>Caricamento...</p>
+    {/if}
+    {#each posts as post}
+      <MenuWineRow {post} />
+    {/each}
+  </table>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    background-image: url(img/vino-icon2.webp);
+    background-repeat: no-repeat;
+    background-position-y: top;
+    background-position-x: 70%;
+  }
+  img {
+    margin: 3rem auto;
+  }
+  table {
+    width: 90%;
+    margin: auto;
+  }
+</style>
