32808de7a25d3b47ca2929819e337383d8cb1a64
Motocarota <motocarota@gmail.com>
2024-05-23 18:51:18 +0200

    fix links

--- src/lib/WineMenu.svelte
+++ src/lib/WineMenu.svelte
@@ -1,11 +1,11 @@
 <script>
   import { Card } from "flowbite-svelte";
   const links = [
-    { id: 'menu', label: 'La Carta', link: '`files/menu.pdf`' },
-    { id: 'pizza', label: 'La Pizzeria', link: '`files/pizza.pdf`' },
-    { id: 'bar', label: 'Bar Menú & Cocktail', link: `files/bar.pdf` },
-    { id: 'wine', label: 'Carta del Vino', link: `menu-vini` },
-    { id: 'special', label: 'Special del giorno - Lo Chef consiglia', link: `menu-special` },
+    { id: 'menu', label: 'La Carta', link: 'files/menu.pdf' },
+    { id: 'pizza', label: 'La Pizzeria', link: 'files/pizza.pdf' },
+    { id: 'bar', label: 'Bar Menú & Cocktail', link: 'files/bar.pdf' },
+    { id: 'wine', label: 'Carta del Vino', link: 'menu-vini' },
+    { id: 'special', label: 'Special del giorno - Lo Chef consiglia', link: 'menu-special' },
   ]
 </script>
 
