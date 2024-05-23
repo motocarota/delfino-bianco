e7f77df5777e5903f26ca4376b19640b8abfc1d6
Motocarota <motocarota@gmail.com>
2024-05-23 18:41:04 +0200

    menu nuovi

--- src/lib/WineMenu.svelte
+++ src/lib/WineMenu.svelte
@@ -1,10 +1,11 @@
 <script>
   import { Card } from "flowbite-svelte";
   const links = [
-    { id: 'menu', label: 'La Carta' },
-    { id: 'pizza', label: 'La Pizzeria' },
-    { id: 'bar', label: 'Bar Menú & Cocktail' },
-    { id: 'wine', label: 'Carta del Vino' },
+    { id: 'menu', label: 'La Carta', link: '`files/menu.pdf`' },
+    { id: 'pizza', label: 'La Pizzeria', link: '`files/pizza.pdf`' },
+    { id: 'bar', label: 'Bar Menú & Cocktail', link: `files/bar.pdf` },
+    { id: 'wine', label: 'Carta del Vino', link: `menu-vini` },
+    { id: 'special', label: 'Special del giorno - Lo Chef consiglia', link: `menu-special` },
   ]
 </script>
 
@@ -12,7 +13,7 @@
   <h1 class="mt-5">Menù</h1>
   <div class="flex justify-center gap-4 flex-wrap">
     {#each links as link}
-      <Card img={`img/${link.id}.webp`} href={`files/${link.id}.pdf`} target="_blank">{link.label}</Card>
+      <Card img={`img/${link.id}.webp`} href={link.link} target="_blank">{link.label}</Card>
     {/each}
   </div>
 </section>
