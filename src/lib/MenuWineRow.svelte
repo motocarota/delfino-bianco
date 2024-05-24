2dac7c46bcd4eb7b44b9bc0ef7f2591987d1ed6c
Motocarota <motocarota@gmail.com>
2024-05-24 11:18:40 +0200

    adds dots

--- src/lib/MenuWineRow.svelte
+++ src/lib/MenuWineRow.svelte
@@ -1,19 +1,22 @@
 <script>
   export let post;
   const [
-    name, type, canteen, location, price
+    name, type, canteen, location, price, qty = 0, color = ""
   ] = post ?? [];
 </script>
 
+{#if qty && qty > 0}
 <tr>
   {#if !price}
-    <th colspan="3" class="pt-5 text-xl">{name}</th>
+    <th colspan="4" class="pt-5 text-xl">{name}</th>
   {:else}
+    <td style="background: {color}; width: 35px; border-radius: 30px"> </td>
     <td><span class="text-xl">{name}</span> {type} - {canteen} - {location}</td>
     <td class="price">{price}</td>
   {/if}
-</tr>
-
+  </tr>
+{/if}
+  
 <style>
   th {
     font-weight: 900;
